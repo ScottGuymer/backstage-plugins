1c487a69f5310d047cca78364ac9d19a5f93794b
Scott Guymer <scott.guymer@philips.com>
2024-08-22 16:53:23 +0200

    Bump to backstage 1.30.x dependencies

--- packages/backend/src/types.ts
+++ packages/backend/src/types.ts
@@ -1,25 +1,24 @@
 import { Logger } from 'winston';
 import { Config } from '@backstage/config';
-import {
-  PluginCacheManager,
-  PluginDatabaseManager,
-  PluginEndpointDiscovery,
-  TokenManager,
-  UrlReader,
-} from '@backstage/backend-common';
-import { PluginTaskScheduler } from '@backstage/backend-tasks';
+import { PluginCacheManager, TokenManager } from '@backstage/backend-common';
 import { PermissionEvaluator } from '@backstage/plugin-permission-common';
 import { IdentityApi } from '@backstage/plugin-auth-node';
+import {
+  DatabaseService,
+  DiscoveryService,
+  SchedulerService,
+  UrlReaderService,
+} from '@backstage/backend-plugin-api';
 
 export type PluginEnvironment = {
   logger: Logger;
-  database: PluginDatabaseManager;
+  database: DatabaseService;
   cache: PluginCacheManager;
   config: Config;
-  reader: UrlReader;
-  discovery: PluginEndpointDiscovery;
+  reader: UrlReaderService;
+  discovery: DiscoveryService;
   tokenManager: TokenManager;
-  scheduler: PluginTaskScheduler;
+  scheduler: SchedulerService;
   permissions: PermissionEvaluator;
   identity: IdentityApi;
 };
